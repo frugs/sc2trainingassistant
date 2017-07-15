c456758bb629ddb71361e54d37e803720579c444
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-07-15 02:04:34 +0100

    proper db path

--- start_training_assistant_webapp.py
+++ start_training_assistant_webapp.py
@@ -1,11 +1,20 @@
+import os
+
 from sc2trainingassistant.webapp import start_webapp
 
 HOST = "localhost"
 PORT = 32332
 
+APP_DATA_PATH = os.path.expanduser("~/sc2trainingassistant")
+
 
 def main():
-    start_webapp(HOST, PORT, "/tmp/blah.json")
+    if not os.path.exists(APP_DATA_PATH):
+        os.makedirs(APP_DATA_PATH)
+
+    db_path = os.path.join(APP_DATA_PATH, "db.json")
+
+    start_webapp(HOST, PORT, db_path)
 
 if __name__ == "__main__":
     main()
